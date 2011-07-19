77ffd2828a340083ba5fa9ccc82a68369aafb7fd
Aaron Iles <aaron.iles@gmail.com>
2011-07-19 21:21:28 +1000

    Refactor main.execute unit tests
    Update test names for main.execute inline with new main.run module.

--- tests/test_run.py
+++ tests/test_run.py
@@ -2,9 +2,9 @@ import unittest
 
 import main
 
-class TestMain(unittest.TestCase):
+class TestExecute(unittest.TestCase):
 
-    def test_main_false(self):
+    def test_execute_false(self):
         # main.execute() return false outside __main__
         self.assertFalse(main.execute())
 
