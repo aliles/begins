015e18991f0dc272ca60decedeff4df749f02072
Aaron Iles <aaron.iles@gmail.com>
2013-01-01 19:07:24 +1100

    Update package metadata
    Modify the package metadata in distutil's setup.py file. Updated
    metadata reflects such things as the change in license and package name
    to 'begins'.

--- setup.py
+++ setup.py
@@ -1,29 +1,50 @@
 from distribute_setup import use_setuptools
 use_setuptools()
 
-from setuptools import setup, find_packages
+from setuptools import setup
+import re
+import sys
+
+
+def load_version(filename='begin/version.py'):
+    "Parse a __version__ number from a source file"
+    with open(filename) as source:
+        text = source.read()
+        match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", text)
+        if not match:
+            msg = "Unable to find version number in {}".format(filename)
+            raise RuntimeError(msg)
+        version = match.group(1)
+        return version
+
+PYTHON3K = sys.version_info[0] > 2
 
 setup(
-    name="main",
-    version="0.1",
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    name="begins",
+    version=load_version(),
+    packages=['begin'],
+    zip_safe=False,
     author="Aaron Iles",
     author_email="aaron.iles@gmail.com",
-    url="http://github.com/aliles/main",
-    download_url="http://github.com/aliles/main/downloads",
-    description="Syntactic sugar for Python's main",
+    url="http://begins.readthedocs.org",
+    description="Syntactic sugar for starting Python programs",
     long_description=open('README.rst').read(),
-    license="PSF",
+    license="ASL",
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Python Software Foundation License',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    test_suite = "tests"
+    tests_require=[] if PYTHON3K else ['unittest2'],
+    test_suite="tests" if PYTHON3K else "unittest2.collector"
 )
