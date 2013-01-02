c174fa3d421592db7bce3607181f7fdd694fd4ff
Aaron Iles <aaron.iles@gmail.com>
2013-01-02 21:17:37 +1100

    Improve compatability with Py2 and Py3
    Use imports from __future__ to make language behaviour as consistent as
    possible between Python2 and Python3.

--- begin/__init__.py
+++ begin/__init__.py
@@ -1,2 +1,3 @@
 "Convenience function for starting Python programs"
+from __future__ import absolute_import, division, print_function
 from begin.main import start
--- begin/main.py
+++ begin/main.py
@@ -1,4 +1,5 @@
 "Syntactic sugar for a programs 'main'."
+from __future__ import absolute_import, division, print_function
 import atexit
 import inspect
 
