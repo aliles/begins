05e61cc182220a0ebe7374b152955ab0ca2fd648
Aaron Iles <aaron.iles@gmail.com>
2013-01-02 22:27:40 +1100

    Expand begin package __init__ module
    Import __version__ from the version module but set __all__ to ensure
    that it isn't imported if using 'import *'.

--- begin/__init__.py
+++ begin/__init__.py
@@ -1,3 +1,6 @@
 "Convenience function for starting Python programs"
 from __future__ import absolute_import, division, print_function
 from begin.main import start
+from begin.version import __version__
+
+__all__ = ['start']
