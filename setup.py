ee13148c1cc84f61cbe2d8478ab74b383d5a14b0
Aaron Iles <aaron.iles@gmail.com>
2013-01-02 21:20:43 +1100

    Add mock package as a requirement for testing
    Mock is added as a dependency to aid in unit test development.

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    tests_require=[] if PYTHON3K else ['unittest2'],
+    tests_require=['mock'] + [] if PYTHON3K else ['unittest2'],
     test_suite="tests" if PYTHON3K else "unittest2.collector"
 )
