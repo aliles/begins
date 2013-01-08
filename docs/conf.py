aeb13e799dccb8ef95094dd25fbd40432b23c30c
Aaron Iles <aaron.iles@gmail.com>
2013-01-08 20:34:21 +1100

    Update copyright date in package documentation.
    Changed date to 2013 from 2012.

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'begins'
-copyright = '2012, Aaron Iles'
+copyright = '2013, Aaron Iles'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
