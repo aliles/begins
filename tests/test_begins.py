886b868433d07d6d8111d454417968cf3dfaf71f
Aaron Iles <aaron.iles@gmail.com>
2013-01-16 22:17:59 +1100

    Mock atexit.register during doctests
    Prevent calling the standard library's atexit.register() function during
    doctest runs of the documentation. This avoids exceptions during the
    interpreter shutdown.

--- tests/test_begins.py
+++ tests/test_begins.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import, division, print_function
+import atexit
+import mock
 
 try:
     import unittest2 as unittest
@@ -15,7 +17,8 @@ class TestTouchstone(unittest.TestCase):
     def test_has_version(self):
         self.assertTrue(begin.__version__)
 
-    def test_readme(self):
+    @mock.patch('atexit.register')
+    def test_readme(self, register):
         doctest.testfile('../README.rst')
 
 
