b0542c85ac4f41f02a534e18688c5c055afe9aca
Aaron Iles <aaron.iles@gmail.com>
2013-05-30 22:06:18 +1000

    Vendor latest funcsigs
    Vendorise the latest version of the funcsigs package inside begins to
    avoid a package dependency.

--- begin/funcsigs/__init__.py
+++ begin/funcsigs/__init__.py
@@ -13,7 +13,8 @@ import types
 try:
     from collections import OrderedDict
 except ImportError:
-    from begin.odict import OrderedDict
+    from begin.funcsigs.odict import OrderedDict
+
 
 __all__ = ['BoundArguments', 'Parameter', 'Signature', 'signature']
 
@@ -28,7 +29,7 @@ _NonUserDefinedCallables = (_WrapperDescriptor,
 
 def formatannotation(annotation, base_module=None):
     if isinstance(annotation, type):
-        if annotation.__module__ in ('builtins', base_module):
+        if annotation.__module__ in ('builtins', '__builtin__', base_module):
             return annotation.__name__
         return annotation.__module__+'.'+annotation.__name__
     return repr(annotation)
