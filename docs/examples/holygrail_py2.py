98958d6062ab5916f3cbd160b4c189773614936b
Aaron Iles <aaron.iles@gmail.com>
2013-07-09 14:58:24 +1000

    Add basic usage examples to examples collection
    Create a new subdirectory below the package documentation to hold the
    existing examples as well as add the existing examples already used in
    the package README file.

---
+++ docs/examples/holygrail_py2.py
@@ -0,0 +1,4 @@
+import begin
+@begin.start
+def run(name='Arther', quest='Holy Grail', colour='blue', *knights):
+    "tis but a scratch!"
---
+++ docs/examples/holygrail_py3.py
@@ -0,0 +1,6 @@
+import begin
+@begin.start
+def run(name: 'What, is your name?',
+        quest: 'What, is your quest?',
+        colour: 'What, is your favourite colour?'):
+    pass
